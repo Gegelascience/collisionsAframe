0b7755c4edb69b88b146078d842f4e7430c3658d
gegelascience <remi.verchere2@gmail.com>
2018-09-30 11:27:07 +0200

    ajout de commentaires

--- scripts/components/cam_move.js
+++ scripts/components/cam_move.js
@@ -2,42 +2,42 @@ AFRAME.registerComponent('camcol', {
     init:function(){
         
         document.addEventListener("keypress",function(e) {
-            //init
-            var cam=document.getElementById("cam")
-            var el=document.getElementById("cambody")
-            var pos=cam.getAttribute("position")
+            //récupération des balises html
+            var cam=document.getElementById("cam");
+            var el=document.getElementById("cambody");
+            var pos=cam.getAttribute("position");
             var camOrientation=(cam.getAttribute("rotation").y)*2*Math.PI/360;
             //ajustement hauteur de la camera
             if (pos.y<0.5) {
-                cam.setAttribute("position",'y',0.5)
+                cam.setAttribute("position",'y',0.5);
             }
             //déplacement de la camera
             if (e.keyCode===38) {//up
                 pos.z-=1*Math.cos(camOrientation);
                 pos.x-=1*Math.sin(camOrientation);
-                cam.setAttribute("position",pos)
-                el.body.position.set(pos.x,pos.y,pos.z)
+                cam.setAttribute("position",pos);
+                el.body.position.set(pos.x,pos.y,pos.z);
 
             }else if(e.keyCode===40){//down
                 pos.z+=1*Math.cos(camOrientation);
                 pos.x+=1*Math.sin(camOrientation);
-                cam.setAttribute("position",pos)
-                el.body.position.set(pos.x,pos.y,pos.z)
+                cam.setAttribute("position",pos);
+                el.body.position.set(pos.x,pos.y,pos.z);
             }else if(e.keyCode===37){//left
                 pos.z-=1*Math.cos(camOrientation+Math.PI/2);
                 pos.x-=1*Math.sin(camOrientation+Math.PI/2);
-                cam.setAttribute("position",pos)
-                el.body.position.set(pos.x,pos.y,pos.z)
+                cam.setAttribute("position",pos);
+                el.body.position.set(pos.x,pos.y,pos.z);
             }
             else if(e.keyCode===39){//right
                 pos.z+=1*Math.cos(camOrientation+Math.PI/2);
                 pos.x+=1*Math.sin(camOrientation+Math.PI/2);
-                cam.setAttribute("position",pos)
-                el.body.position.set(pos.x,pos.y,pos.z)
+                cam.setAttribute("position",pos);
+                el.body.position.set(pos.x,pos.y,pos.z);
             //effacer le texte
             }else if (e.keyCode===9) {//tab
-                var text=document.getElementById("info")
-                text.setAttribute("value","")
+                var text=document.getElementById("info");
+                text.setAttribute("value","");
             }
 
             
